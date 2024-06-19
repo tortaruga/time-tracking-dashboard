f8c23aa89deccab8494411b27b6476d81768d88e
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-19 12:28:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 import data from './data.json' assert { type: 'json' };
 
-const work = document.getElementById('work');
-console.log('hello')
+const currentStats = document.querySelectorAll('.current');
+const previousStats = document.querySelectorAll('.previous');
+
+for (let i = 0; i < data.length; i++) {
+    currentStats.forEach(stat => {
+        stat.textContent = data[i].timeframes.weekly.current
+    })
+}
+
+console.log(45)
+
+document.getElementById('work').innerHTML = data[1].timeframes.weekly.current
