719947d8dc94d5d4d97064cdc04c53f15f85b8ba
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-19 11:28:08 +0200

    updated index.html

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import data from './data.json' assert { type: 'json' };
 
 const work = document.getElementById('work');
-work.innerHTML = data[0].title;
-console.log('pak jinyeong jinjja jalsenggyeotta')
+console.log('hello')
